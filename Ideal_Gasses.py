0e0707fab6aa82e1e7625828ffc97ed1af244104
TristanMelfi13 <110581919+TristanMelfi13@users.noreply.github.com>
2022-12-25 23:12:13 -0500

    Update Ideal_Gasses.py

--- Ideal_Gasses.py
+++ Ideal_Gasses.py
@@ -10,18 +10,21 @@ def Distance(x1, y1, x2, y2):
     return math.sqrt(math.pow(x1 - x2, 2) + math.pow(y1 - y2, 2))
 
 
-def RMSVelocity(Temp, MolarMass):
+def MostProbableVelocity(Temp, MolarMass):
     MolarMass /= 1000
-    return math.sqrt((Temp * 8.3145 * 3) / MolarMass)
+    return math.sqrt((Temp * 8.3145 * 2) / MolarMass)
+
+
+def LJTest(Distance):
+    pass
 
 
 class Particle:
-    def __init__(self, Xcor, Ycor, XVel, YVel, CollisionFlag, MolarMass):
+    def __init__(self, Xcor, Ycor, XVel, YVel, MolarMass):
         self.Xcor = Xcor
         self.Ycor = Ycor
         self.XVelocity = XVel
         self.YVelocity = YVel
-        self.CollisionFlag = CollisionFlag
         self.MolarMass = MolarMass
         self.Visual = turtle.Turtle()
         self.Visual.shape("circle")
@@ -32,8 +35,8 @@ class Particle:
         self.Visual.penup()
         self.Visual.goto(self.Xcor, self.Ycor)
         DistanceFromOrigin = Distance(self.Visual.xcor(), 0, self.Visual.ycor(), 0)
-        Accel = RMSVelocity(300, self.MolarMass)
-        AccelTest = RMSVelocity(300, self.MolarMass) / 1000
+        Accel = MostProbableVelocity(298, self.MolarMass) + random() * (5 + 15) - 15
+        print("Most Probable Speed of Helium atom at 300 Kelvin: {}".format(Accel))
         self.XVelocity += (Accel * (self.Visual.xcor() / DistanceFromOrigin)) / 1000
         self.YVelocity += (Accel * (self.Visual.ycor() / DistanceFromOrigin)) / 1000
 
@@ -54,57 +57,59 @@ class Particle:
             if j != Iter:
                 Distance = math.sqrt(math.pow(PList[j].Visual.xcor() - self.Visual.xcor(), 2) +
                                      math.pow(PList[j].Visual.ycor() - self.Visual.ycor(), 2))
-                if self.CollisionFlag != 0:
-                    self.CollisionFlag -= 1
-                if Distance < 8 and self.CollisionFlag == 0:
+                if Distance < 8:
                     XVelSave = self.XVelocity
                     YVelSave = self.YVelocity
                     self.XVelocity = PList[j].XVelocity
                     self.YVelocity = PList[j].YVelocity
                     PList[j].XVelocity = XVelSave
                     PList[j].YVelocity = YVelSave
-                    self.CollisionFlag = 15
 
 
 def CreateNewParticle():
     NewParticle = Particle(randrange(-1 * int(ScreenSize[0] / 2), int(ScreenSize[0] / 2)),
                            randrange(-1 * int(ScreenSize[1] / 2), int(ScreenSize[1] / 2)),
-                           random() * (1 + 1) - 1, random() * (1 + 1) - 1, 0, 4)
+                           random() * (1 + 1) - 1, random() * (1 + 1) - 1, 4)
     if NewParticle.Xcor > 0:
-        NewParticle.Xcor -= 100
+        NewParticle.Xcor -= 50
     else:
-        NewParticle.Xcor += 100
+        NewParticle.Xcor += 50
 
     if NewParticle.Ycor > 0:
-        NewParticle.Ycor -= 100
+        NewParticle.Ycor -= 50
     else:
-        NewParticle.Ycor += 100
+        NewParticle.Ycor += 50
 
     NewParticle.InitializeTurtle()
     print("New Particle Created:")
-    print("Xcor: {} || Ycor: {} || XVel: {} || YVel: {} ||".format(NewParticle.Xcor, NewParticle.Ycor,
-                                                                   NewParticle.XVelocity, NewParticle.YVelocity))
+    print("Xcor: {} || Ycor: {} || XVel: {} || YVel: {} || Magnitude of Vector {} m/s".format(NewParticle.Xcor,
+                                                                                              NewParticle.Ycor,
+                                                                                              NewParticle.XVelocity,
+                                                                                              NewParticle.YVelocity,
+                                                                                              1000 * (math.sqrt(
+                                                                                                  math.pow(
+                                                                                                      NewParticle.XVelocity,
+                                                                                                      2) + math.pow(
+                                                                                                      NewParticle.YVelocity,
+                                                                                                      2)))))
     NewParticle.Visual.penup()
     return NewParticle
 
 
 ParticleList = []
 
-for i in range(5):
+for i in range(18):
     ParticleList.append(CreateNewParticle())
 
 wn = turtle.Screen()
 wn.setup(width=ScreenSize[0], height=ScreenSize[1])
 
+sum = 0
 
-def HopefullyTempEqualsWhateverItsSetTo(PList):
-    sum = 0
-    for i in range(len(PList)):
-        sum += (0.5) * (6.6422E-24) * math.pow((math.sqrt((math.pow(PList[i].XVelocity, 2) + math.pow(PList[i].YVelocity, 2)))), 2)
-    print(sum / len(PList))
+for i in range(len(ParticleList)):
+    sum = 1000 * ((math.pow(ParticleList[i].XVelocity, 1) + math.pow(ParticleList[i].YVelocity, 1)))
 
-
-HopefullyTempEqualsWhateverItsSetTo(ParticleList)
+print("SUM OF PARTICLE VELOCITIES {}".format(sum / len(ParticleList)))
 
 while True:
     XVelSum = 0
@@ -117,4 +122,3 @@ while True:
         ParticleList[i].DetectWallCollision()
         ParticleList[i].CheckForParticleCollision(i, ParticleList)
         turtle.update()
-
